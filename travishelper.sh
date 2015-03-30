95c894023610dfff203b28a8706bc2817b8661fb
David Volm <david@volminator.com>
2015-03-30 08:49:59 -0500

    ls /usr/bin

--- travishelper.sh
+++ travishelper.sh
@@ -4,6 +4,7 @@
 
 echo CXX: $1
 
+#gcc 4.8
 if [ "$1" == "g++" ]; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
@@ -11,9 +12,11 @@ if [ "$1" == "g++" ]; then
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 fi
 
+#clang 3.5
 if [ "$1" == "clang++" ]; then
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo echo deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main >> /etc/apt/sources.list
     sudo echo deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main >> /etc/apt/sources.list
     apt-get install clang-3.5 clang-3.5-doc libclang-common-3.5-dev libclang-3.5-dev libclang1-3.5 libclang1-3.5-dbg libllvm-3.5-ocaml-dev libllvm3.5 libllvm3.5-dbg lldb-3.5 llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime clang-modernize-3.5 clang-format-3.5 python-clang-3.5 lldb-3.5-dev
+    ls /usr/bin
 fi
