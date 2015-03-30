26f2ee857a7353c471694704a5006f313f4cd21b
David Volm <david@volminator.com>
2015-03-30 16:16:22 -0500

    no ls

--- travishelper.sh
+++ travishelper.sh
@@ -25,6 +25,5 @@ if [ "$1" == "clang++" ]; then
     sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -qq
     sudo apt-get install -qq clang-3.5
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.5 90
-    ls /usr/bin
     clang++ --version
 fi
