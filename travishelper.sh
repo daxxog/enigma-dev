1a535379c8c2b15647db3cb3963918407f6be901
David Volm <david@volminator.com>
2015-03-30 12:28:28 -0500

    stop pg

--- travishelper.sh
+++ travishelper.sh
@@ -4,9 +4,11 @@
 
 echo CXX: $1
 
+sudo pg_ctl stop
+
 sudo add-apt-repository -y ppa:h-rayflood/gcc-upper
 sudo apt-get update -qq
-sudo apt-get -o Dpkg::Options::="--force-confnew" upgrade -qq
+sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -qq
 
 #gcc 4.9
 if [ "$1" == "g++" ]; then
@@ -18,7 +20,7 @@ fi
 if [ "$1" == "clang++" ]; then
     sudo add-apt-repository -y ppa:h-rayflood/llvm-upper
     sudo apt-get update -qq
-    sudo apt-get -o Dpkg::Options::="--force-confnew" upgrade -qq
+    sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -qq
     sudo apt-get install -qq llvm-3.5
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.5 90
     clang++ --version
