2ac590a4b50dfb7668f62b59b63602d69d7e671f
David Volm <david@volminator.com>
2015-03-30 08:41:46 -0500

    test

--- travishelper.sh
+++ travishelper.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# - travishelper.sh
+# - helps set up travis with the proper gcc/clang
+
 echo CXX: $1
+
 if [ "$1" == "g++" ]; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
     sudo apt-get install -qq gcc-4.8 g++-4.8
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+fi
+
+if [ "$1" == "clang++" ]; then
+    sudo cat /etc/apt/sources.list 
 fi
