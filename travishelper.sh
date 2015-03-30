2f6a9028e6f6652e99a84cfeb38d9d5d1b8da665
David Volm <david@volminator.com>
2015-03-30 12:05:29 -0500

    --force-confnew

--- travishelper.sh
+++ travishelper.sh
@@ -4,13 +4,15 @@
 
 echo CXX: $1
 
+sudo rm /etc/rsyslog.conf
+
 sudo add-apt-repository -y ppa:h-rayflood/gcc-upper
 sudo apt-get update -qq
-sudo apt-get dist-upgrade -qq
+sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -qq
 
 #gcc 4.9
 if [ "$1" == "g++" ]; then
-    yes | sudo  apt-get install -qq gcc-4.9 g++-4.9
+    sudo  apt-get install -qq gcc-4.9 g++-4.9
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
 fi
 
@@ -18,8 +20,8 @@ fi
 if [ "$1" == "clang++" ]; then
     sudo add-apt-repository -y ppa:h-rayflood/llvm-upper
     sudo apt-get update -qq
-    sudo apt-get dist-upgrade -qq
-    yes | sudo apt-get install -qq llvm-3.5
+    sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -qq
+    sudo apt-get install -qq llvm-3.5
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.5 90
     clang++ --version
 fi
