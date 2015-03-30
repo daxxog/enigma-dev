0dfee3866bccfa8db179e569554b577f43840d67
David Volm <david@volminator.com>
2015-03-30 09:38:37 -0500

    common

--- travishelper.sh
+++ travishelper.sh
@@ -17,6 +17,7 @@ if [ "$1" == "clang++" ]; then
     sudo apt-get update
     sudo echo deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main >> /etc/apt/sources.list
     sudo echo deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main >> /etc/apt/sources.list
+    sudo echo deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu precise main >> /etc/apt/sources.list
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update
     apt-get install clang-3.5 lldb-3.5
