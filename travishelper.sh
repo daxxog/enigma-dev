06ab0414023a2f17addd68255d3ad08e95124077
David Volm <david@volminator.com>
2015-03-30 08:22:12 -0500

    travishelper

---
+++ travishelper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$CXX" == "g++" ]; then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qq
+    sudo apt-get install -qq gcc-4.8 g++-4.8
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+fi
